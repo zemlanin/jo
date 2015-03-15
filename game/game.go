b9ff8df9168bc106d726b8d714e699b0dbbbf79a
Anton Verinov <git@zemlan.in>
2015-03-15 17:11:47 +0200

    game.go

---
+++ game/game.go
@@ -0,0 +1,45 @@
+package game
+
+import (
+// "log"
+)
+
+type lazyJson map[string]interface{}
+type gameField map[string]int
+
+type PlayerInput struct {
+	Game_id   string
+	Value     int
+	Timestamp int
+}
+
+var game_field = gameField{
+	"x": 0,
+	"y": 2,
+}
+
+func GetState(game_id string) lazyJson {
+	game_state := lazyJson{
+		"gameField": game_field,
+		"gameId":    game_id,
+	}
+
+	return game_state
+}
+
+func InterpretInput(input PlayerInput) {
+	switch input.Value {
+	case 0:
+		game_field["y"] = game_field["y"] + 1
+
+	case 1:
+		game_field["y"] = game_field["y"] - 1
+
+	case 2:
+		game_field["x"] = game_field["x"] - 1
+
+	case 3:
+		game_field["x"] = game_field["x"] + 1
+
+	}
+}
--- wsroute.go
+++ wsroute.go
@@ -1,64 +1,80 @@
 package main
 
 import (
+	"github.com/mitchellh/mapstructure"
+	"jo/game"
 	"log"
 )
 
 type lazyJson map[string]interface{}
-type wsMessage struct{
-	Type string `json:"type"`
+type wsMessage struct {
+	Type    string      `json:"type"`
 	Payload interface{} `json:"payload"`
 }
 
 func routeMessage(message wsMessage) (wsMessage, wsMessage, error) {
 	var private wsMessage
 	var public wsMessage
+	var ok bool
+	var incoming map[string]interface{}
+	var game_id string
+
+	if incoming, ok = message.Payload.(map[string]interface{}); !ok {
+		return private, public, nil
+	}
+
+	if game_id, ok = incoming["gameId"].(string); !ok {
+		return private, public, nil
+	}
 
 	log.Println(message)
 	switch message.Type {
 	case "CONNECT_PLAYER":
 		player := lazyJson{
 			"playerId": "12",
-			"gameId":   2222,
+			"gameId":   game_id,
 			"name":     "whatever",
 			"online":   true,
 		}
 		private = wsMessage{
-			Type: "PLAYER",
+			Type:    "PLAYER",
 			Payload: player,
 		}
 
 	case "GET_PLAYERS":
 		players := []lazyJson{
 			{
-				"gameId": 2222,
+				"gameId": game_id,
 				"name":   "whatever",
 				"online": true,
 			},
 			{
-				"gameId": 2222,
+				"gameId": game_id,
 				"name":   "another",
 				"online": false,
 			},
 		}
 
 		private = wsMessage{
-			Type: "PLAYERS",
+			Type:    "PLAYERS",
 			Payload: players,
 		}
 
 	case "GET_GAME_STATE":
-		game_field := lazyJson{
-			"x": 0,
-			"y": 2,
-		}
-		game_state := lazyJson{
-			"gameField": game_field,
-			"gameId":    2222,
-		}
 		public = wsMessage{
-			Type: "GAME_STATE",
-			Payload: game_state,
+			Type:    "GAME_STATE",
+			Payload: game.GetState(game_id),
+		}
+
+	case "NEW_PLAYERS_INPUT":
+		var input game.PlayerInput
+		if err := mapstructure.Decode(incoming, &input); err != nil {
+			panic(err)
+		}
+		game.InterpretInput(input)
+		public = wsMessage{
+			Type:    "GAME_STATE",
+			Payload: game.GetState(game_id),
 		}
 	}
 
